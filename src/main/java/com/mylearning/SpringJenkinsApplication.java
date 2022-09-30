dc53a6573ba33535fbdfb6818a887dc2ee3bce0a
Pratheush <pratheush@outlook.com>
2022-09-30 11:06:58 +0530

    first commit

---
+++ src/main/java/com/mylearning/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.mylearning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	private static final Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	public static void main(String[] args) {
+		logger.info("Spring Main Application");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+	public Integer add(Integer num1,Integer num2) {
+		return num1+num2;
+	}
+	
+	public Integer multiply(Integer num1,Integer num2) {
+		return num1*num2;
+	}
+}
---
+++ src/test/java/com/mylearning/SpringJenkinsApplicationTests.java
@@ -0,0 +1,29 @@
+package com.mylearning;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	private static final Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	SpringJenkinsApplication app=new SpringJenkinsApplication();
+	
+	@Test
+	void addTest() {
+		logger.info("Spring Test ApplicationTests Addition");
+		assertEquals(4, app.add(2, 2));
+	}
+	
+	@Test
+	void multiplyTest() {
+		logger.info("Spring Test ApplicationTests Multiply");
+		assertEquals(6, app.multiply(3, 2));
+	}
+
+}
