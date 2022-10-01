2383ae72eebde05e3331cb1ddb5f0be66b566db6
Pratheush <pratheush@outlook.com>
2022-10-01 10:06:52 +0530

    used assertThat in test cast substract

--- src/test/java/com/mylearning/SpringJenkinsApplicationTests.java
+++ src/test/java/com/mylearning/SpringJenkinsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mylearning;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -30,5 +31,6 @@ class SpringJenkinsApplicationTests {
 	void substractTest() {
 		logger.info("Spring Test ApplicationTests Substract");
 		assertEquals(1, app.substract(3, 2));
+		assertThat(app.substract(5, 3)).isEqualTo(2);
 	}
 }
